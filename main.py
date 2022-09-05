ff818c3dc132d730b654e0e91b57c3dfe97fc429
alicez <jyz2003@cs.washington.edu>
2022-09-05 13:37:00 -0400

    Update: print out song title in console for faster dev debugging

--- main.py
+++ main.py
@@ -113,6 +113,7 @@ async def round(ctx):
     vc.stop()
 
     await ctx.send("What is the title of this song?")
+    print("The title of the song is " + song_key[selected_song_url].title)
 
     def check(message):  # check if the message was sent by one of the players and if their guess is the right answer
         if message.author in players and message.content.lower() == song_key[selected_song_url].title.lower():
